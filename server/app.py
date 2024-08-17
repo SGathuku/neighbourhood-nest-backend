5e8f4011973c15f1dd394a4ac16e373575726f45
Gathuku Samwel <gathsam3446@gmail.com>
2024-08-17 12:07:32 +0300

    SuperAdmin Routes

--- server/app.py
+++ server/app.py
@@ -53,3 +53,50 @@ class AdminNewsResource(Resource):
         db.session.delete(news)
         db.session.commit()
         return {"message": "News deleted"}
+
+
+class SuperAdminAdminResource(Resource):
+    def get(self, super_admin_id):
+        super_admin = SuperAdmin.query.get_or_404(super_admin_id)
+        admins = Admin.query.all()
+        return jsonify([admin.to_dict() for admin in admins])
+
+    def post(self, super_admin_id):
+        super_admin = SuperAdmin.query.get_or_404(super_admin_id)
+        data = request.json
+        new_admin = Admin(
+            name=data['name'],
+            email=data['email'],
+            url=data['url'],
+            neighborhood_id=data['neighborhood_id']
+        )
+        db.session.add(new_admin)
+        db.session.commit()
+        return jsonify(new_admin.to_dict()), 201
+
+    def delete(self, super_admin_id, admin_id):
+        super_admin = SuperAdmin.query.get_or_404(super_admin_id)
+        admin = Admin.query.get_or_404(admin_id)
+        db.session.delete(admin)
+        db.session.commit()
+        return {"message": "Admin deleted"}
+
+class SuperAdminNeighborhoodResource(Resource):
+    def post(self, super_admin_id):
+        super_admin = SuperAdmin.query.get_or_404(super_admin_id)
+        data = request.json
+        new_neighborhood = Neighborhood(
+            name=data['name'],
+            location=data['location'],
+            image_url=data['image_url']
+        )
+        db.session.add(new_neighborhood)
+        db.session.commit()
+        return jsonify(new_neighborhood.to_dict()), 201
+
+    def delete(self, super_admin_id, neighborhood_id):
+        super_admin = SuperAdmin.query.get_or_404(super_admin_id)
+        neighborhood = Neighborhood.query.get_or_404(neighborhood_id)
+        db.session.delete(neighborhood)
+        db.session.commit()
+        return {"message": "Neighborhood deleted"}
