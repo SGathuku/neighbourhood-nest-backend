b3beae450ffd7a6b862861a70d80ad1f16b883b6
Gathuku Samwel <gathsam3446@gmail.com>
2024-08-17 12:14:11 +0300

    Adding News Routes

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from flask_restful import Resource
-from models import Admin, Resident, News, Event, db
+from models import SuperAdmin, Admin, Resident, News, Event, Neighborhood, db
 
 class AdminResidentsResource(Resource):
     def get(self, admin_id):
@@ -100,3 +100,39 @@ class SuperAdminNeighborhoodResource(Resource):
         db.session.delete(neighborhood)
         db.session.commit()
         return {"message": "Neighborhood deleted"}
+
+
+class NewsResource(Resource):
+    def get(self, news_id):
+        news = News.query.get_or_404(news_id)
+        return jsonify(news.to_dict())
+
+    def put(self, news_id):
+        news = News.query.get_or_404(news_id)
+        data = request.json
+        news.title = data['title']
+        news.description = data['description']
+        db.session.commit()
+        return jsonify(news.to_dict())
+
+    def delete(self, news_id):
+        news = News.query.get_or_404(news_id)
+        db.session.delete(news)
+        db.session.commit()
+        return {"message": "News deleted"}
+
+class NewsListResource(Resource):
+    def get(self):
+        news_list = News.query.all()
+        return jsonify([news.to_dict() for news in news_list])
+
+    def post(self):
+        data = request.json
+        new_news = News(
+            title=data['title'],
+            description=data['description'],
+            date_created=datetime.utcnow()
+        )
+        db.session.add(new_news)
+        db.session.commit()
+        return jsonify(new_news.to_dict()), 201
